c4a84eb92cb007349635d03353bee12562a66b65
better-nine <76273087+better-nine@users.noreply.github.com>
2025-05-03 01:46:41 +0900

    add @EnableAsync

--- src/main/java/com/example/LegacyApplication.java
+++ src/main/java/com/example/LegacyApplication.java
@@ -1,8 +1,10 @@
 package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class LegacyApplication {
     public static void main(String[] args) {
         SpringApplication.run(LegacyApplication.class, args);
