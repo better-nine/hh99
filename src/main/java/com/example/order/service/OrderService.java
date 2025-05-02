1373fde3628638e14dc1520f51a94f100d908ad8
better-nine <76273087+better-nine@users.noreply.github.com>
2025-05-03 00:04:21 +0900

    update create order

--- src/main/java/com/example/order/service/OrderService.java
+++ src/main/java/com/example/order/service/OrderService.java
@@ -2,10 +2,9 @@ package com.example.order.service;
 
 import com.example.order.dto.OrderDto;
 import com.example.order.entity.Order;
-import com.example.order.entity.OrderStatus;
 import com.example.order.repository.OrderRepository;
+import com.example.orderProduct.service.OrderProductService;
 import com.example.stock.service.StockService;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,11 +13,14 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+
 @Service
 @RequiredArgsConstructor
 public class OrderService {
+
     private final OrderRepository orderRepository;
     private final StockService stockService;
+    private final OrderProductService orderProductService;
 
     /**
      * 주문 등록
@@ -27,62 +29,21 @@ public class OrderService {
      */
     @Transactional(rollbackOn = Exception.class)
     public Order createOrder(OrderDto dto) {
+        String orderId = UUID.randomUUID().toString();
         Order order = Order.builder()
-                .orderId(UUID.randomUUID().toString())
-                .storeId(dto.getStoreId())
-                .productId(dto.getProductId())
-                .stockId(dto.getStockId())
-                .quantity(dto.getQuantity())
+                .orderId(orderId)
+                .status(dto.getStatus())
+                .orderDateTime(Timestamp.valueOf(LocalDateTime.now()))
                 .build();
+        orderRepository.save(order);
 
-        // 재고 감소
-        stockService.decreaseStock(dto.getStockId(), dto.getQuantity());
-        return orderRepository.save(order);
+        // 재고 감소 및 주문 상품 등록
+        dto.getOrderProduct().forEach(orderProductDto -> {
+            stockService.decreaseStock(orderProductDto.getProductId(), orderProductDto.getQuantity());
+            orderProductService.createOrderProduct(orderId, orderProductDto);
+        });
+
+        return order;
     }
 
-    /**
-     * 주문 조회
-     * @param orderId
-     * @return
-     */
-    public Order getOrder(String orderId) {
-        return orderRepository.findByOrderId(orderId)
-                .orElseThrow(() -> new RuntimeException("Order not found"));
-    }
-
-    /**
-     * 주문 수정
-     * @param orderId
-     * @param dto
-     * @return
-     * @throws JsonMappingException
-     */
-    @Transactional
-    public Order updateOrder(String orderId, OrderDto dto) throws JsonMappingException {
-        Order order = orderRepository.findByOrderId(orderId)
-                .orElseThrow(() -> new IllegalArgumentException("Order not found"));
-
-        OrderStatus currentStatus = order.getStatus();
-        OrderStatus newStatus = dto.getStatus();
-
-        if (!currentStatus.canTransitionTo(newStatus)) {
-            throw new IllegalStateException("Can't transition to " + newStatus + " to " + currentStatus);
-        }
-
-        order.setStatus(newStatus);
-        order.setOrderDateTime(Timestamp.valueOf(LocalDateTime.now()));
-
-        return orderRepository.save(order);
-    }
-
-    /**
-     * 주문 삭제
-     * @param orderId
-     */
-    public void deleteOrder(String orderId) {
-        Order order = orderRepository.findByOrderId(orderId)
-                .orElseThrow(() -> new IllegalArgumentException("Order not found"));
-
-        orderRepository.delete(order);
-    }
 }
